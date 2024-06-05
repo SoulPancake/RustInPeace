933ee51e7e7ccc27ca7b32bcfc258c095065f348
Anurag Bandyopadhyay <angbpy@gmail.com>
2024-06-05 12:30:07 +0530

    feat: add options for top domains for pinging

--- rusterm/src/main.rs
+++ rusterm/src/main.rs
@@ -1,6 +1,19 @@
-use dialoguer::{theme::ColorfulTheme, Select};
+use dialoguer::{theme::ColorfulTheme, Select, Input};
 
 fn main() {
+    let top_domains = [
+        "google.com",
+        "facebook.com",
+        "youtube.com",
+        "twitter.com",
+        "amazon.com",
+        "linkedin.com",
+        "instagram.com",
+        "pinterest.com",
+        "reddit.com",
+        "wikipedia.org",
+    ];
+
     loop {
         let selection = Select::with_theme(&ColorfulTheme::default())
             .with_prompt("Select an option:")
@@ -11,12 +24,28 @@ fn main() {
 
         match selection {
             0 => println!("ฅ^•ﻌ•^ฅ Hello!"),
-            1 => println!("Pinging..."),
+            1 => {
+                let ping_selection = Select::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Select a domain to ping:")
+                    .items(&top_domains)
+                    .default(0)
+                    .interact()
+                    .unwrap();
+
+                let domain = if ping_selection < top_domains.len() {
+                    top_domains[ping_selection].to_string()
+                } else {
+                    let custom_domain: String = Input::new()
+                        .with_prompt("Enter a domain to ping:")
+                        .interact()
+                        .unwrap();
+                    custom_domain
+                };
+
+                println!("Pinging {}...", domain);
+            },
             2 => println!("Checking internet speed..."),
-            3 => {
-                println!("Exiting program.");
-                break;
-            }
+            3 => break,
             _ => println!("Invalid option, please try again."),
         }
     }
